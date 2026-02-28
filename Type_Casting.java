d71223e2f1920ed14659a3080f78b8d8529f6600
VAISHNAVI BANSODE <vaishnavibansode004@gmail.com>
2026-02-28 18:44:54 +0530

    Type Casting

---
+++ Type_Casting.java
@@ -0,0 +1,16 @@
+public class Type_Casting {
+    public static void main(String[] args) {
+
+        int i = 50000;
+        float f = 5.25645f;
+        char c = 'V';
+        byte b = 65;
+        short s = 568;
+        long l = 85256L;
+        double d = 5.521;
+
+        double equatuion = (i*b) + (f+c) - (l/s) /d ;
+        System.out.println((i*b) +"  "+ (f+c) +"  "+ (l/s) +"  " + d);
+        System.out.println(equatuion);
+    }
+}
